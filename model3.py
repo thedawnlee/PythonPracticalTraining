95202c01f85dc2509827de26411f85343db76857
thedawnlee <39146624+thedawnlee@users.noreply.github.com>
2019-06-19 18:26:31 +0800

    Delete model3.py

--- model3.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2019/6/18 08:46
-# @Author  : Dawn Lee
-# @Email   : lisantao_tao@outlook.com
-# @File    : model3.py
-# @Software: PyCharm
-
-def test():
-    print('hello python')
